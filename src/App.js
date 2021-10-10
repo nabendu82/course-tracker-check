4ba84e54ad342a56044282d9fc0f5c29a64c513f
Nabendu Biswas <nabendu.biswas@gmail.com>
2021-10-10 11:12:17 +0530

    Final Check

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import CourseForm from './components/CourseForm';
+import CourseList from './components/CourseList';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [courses, setCourses] = useState([]);
+  
+  const loadCourses = async () => {
+    try {
+      const res = await fetch('/api/courses');
+      const courses = await res.json();
+      setCourses(courses);
+    } catch (error) {
+        console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    loadCourses();
+  }, []);
+  
+  return (
+    <div className="container mt-5">
+          <h1 className="mb-5 text-center">Course Tracker</h1>
+          <CourseForm courseAdded={loadCourses} />
+          <CourseList courses={courses} refreshCourses={loadCourses} />
+    </div>
+  );
+}
+
+export default App;
